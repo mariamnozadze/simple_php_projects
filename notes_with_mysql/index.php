ff865dc07be8a2f5721061d77cd1c6417a4449db
mariamnozadze <mariamnozadze97@yahoo.com>
2022-10-05 16:22:51 +0400

    - Created database in mysql - Fetched notes from database - Added function to display saved notes - Fixed Saved function to update notes

--- notes_with_mysql/index.php
+++ notes_with_mysql/index.php
@@ -1,4 +1,23 @@
 <?php
+
+$connection = require_once './Connection.php';
+
+
+$notes = $connection->getNotes();
+
+// echo '<br>';
+// var_dump($notes);
+
+$currentNote = [
+    'id' => '',
+    'title' => '',
+    'description' => ''
+];
+
+if (isset($_GET['id'])) {
+    $currentNote = $connection->getNoteById($_GET['id']);
+}
+
 ?>
 
 <!DOCTYPE html>
@@ -14,22 +33,35 @@
 
 <body>
     <div>
-        <form class="new-note" action="create.php" method="post">
-            <input type="text" name="title" placeholder="Note title" autocomplete="off">
-            <textarea name="description" cols="30" rows="4" placeholder="Note Description"></textarea>
-            <button>New note</button>
+        <form class="new-note" action="save.php" method="post">
+            <input type="hidden" name="id" value="<?php echo $currentNote['id'] ?>">
+            <input type="text" name="title" placeholder="Note title" autocomplete="off" value="<?php echo $currentNote['title'] ?>">
+            <textarea name="description" cols="30" rows="4" placeholder="Note Description">
+                <?php echo $currentNote['description'] ?>
+        </textarea>
+            <button>
+                <?php if ($currentNote['id']) : ?>
+                    Update Note
+                <?php else : ?>
+                    New Note
+                <?php endif; ?>
+            </button>
         </form>
         <div class="notes">
-            <!-- <div class="note">
-            <div class="title">
-                <a href="">Sample note</a>
-            </div>
-            <div class="description">
-                Sample note description
-            </div>
-            <small>15/02/20 19:00:00</small>
-            <button class="close">X</button>
-        </div> -->
+            <?php foreach ($notes as $note) : ?>
+                <div class="note">
+                    <div class="title">
+                        <a href="?id=<?php echo $note['id'] ?>">
+                            <?php echo $note['title'] ?>
+                        </a>
+                    </div>
+                    <div class="description">
+                        <?php echo $note['description'] ?>
+                    </div>
+                    <small><?php echo $note['create_date'] ?></small>
+                    <button class="close">X</button>
+                </div>
+            <?php endforeach; ?>
         </div>
     </div>
 </body>
