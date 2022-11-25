d6b11934f3984ee1f4a005efb8b802ca8230d599
mariamnozadze <mariamnozadze97@yahoo.com>
2022-11-25 22:09:38 +0400

    - Created form to practice - Validating inputs

---
+++ object_oriented_php/validation_1/index.php
@@ -0,0 +1,35 @@
+<?php
+if (isset($_POST['submit'])) {
+    // validate entries
+    echo 'form submitted';
+}
+?>
+
+
+<html lang="en">
+
+<head>
+    <title>validation</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+
+<body>
+
+    <div class="new-user">
+        <h2>Create a new user</h2>
+        <form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="POST">
+
+            <label>username: </label>
+            <input type="text" name="username">
+
+            <label>email: </label>
+            <input type="text" name="email">
+
+            <input type="submit" value="submit" name="submit">
+
+        </form>
+    </div>
+
+</body>
+
+</html>
---
+++ object_oriented_php/validation_2/index.php
@@ -0,0 +1,47 @@
+<?php
+
+require('user_validator.php');
+
+$errors = [];
+
+if (isset($_POST['submit'])) {
+    // validate entries
+    $validation = new UserValidator($_POST);
+    $errors = $validation->validateForm();
+
+    // if errors is empty --> save data to db
+}
+
+?>
+
+<html lang="en">
+
+<head>
+    <title>Form</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+
+<body>
+
+    <div class="new-user">
+        <h2>Create a new user</h2>
+        <form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="POST">
+
+            <label>username: </label>
+            <input type="text" name="username" value="<?php echo htmlspecialchars($_POST['username']) ?? '' ?>">
+            <div class="error">
+                <?php echo $errors['username'] ?? '' ?>
+            </div>
+            <label>email: </label>
+            <input type="text" name="email" value="<?php echo htmlspecialchars($_POST['email']) ?? '' ?>">
+            <div class="error">
+                <?php echo $errors['email'] ?? '' ?>
+            </div>
+            <input type="submit" value="submit" name="submit">
+
+        </form>
+    </div>
+
+</body>
+
+</html>
