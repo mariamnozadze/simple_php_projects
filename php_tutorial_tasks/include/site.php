1b06b1311aa6162bb16cf35ad6aac73346633395
mariamnozadze <mariamnozadze97@yahoo.com>
2022-12-08 22:37:05 +0400

    - Practiced with include files

---
+++ php_tutorial_tasks/include/site.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <title>site</title>
+</head>
+
+<body>
+    <?php include "header.html" ?>
+    <p>hello world</p>
+    <?php include "footer.html" ?>
+</body>
+
+</html>
+</body>
+
+</html>
